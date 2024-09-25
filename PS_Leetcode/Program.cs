2213a7ce4c54fb4682de14ef179b1137607acb4c
mahmoudramzy72 <mahmoudhamdyramzy72@gmail.com>
2024-09-25 16:46:05 +0300

    Add project files.

---
+++ PS_Leetcode/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PS_Leetcode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Problem Solving!");
+        }
+    }
+}
