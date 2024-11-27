d47c3ee6bcf2a36c7e3fc1fd89393f6cb65a1035
Mahmoud Ramzy <mahmoudhamdyramzy72@gmail.com>
2024-11-27 22:16:47 +0200

    Final Value of Variable After Performing Operations

---
+++ PS_Leetcode/Final_Value_Of_Variable_After_Performing_Operations.cs
@@ -0,0 +1,32 @@
+﻿namespace PS_Leetcode
+{
+    // There is a programming language with only four operations and one variable X:
+    // ++X and X++ increments the value of the variable X by 1.     
+    // --X and X-- decrements the value of the variable X by 1. 
+    internal class Final_Value_Of_Variable_After_Performing_Operations
+    {
+        public static void Run()
+        {
+            string[] operations = ["--X", "X++", "X++"];
+            int result = FinalValueAfterOperations(operations);
+            Console.WriteLine(result);
+        }
+
+        public static int FinalValueAfterOperations(string[] operations)
+        {
+            int X = 0;
+            for (int i = 0; i < operations.Length; i++)
+            {
+                if (operations[i] == "++X" || operations[i] == "X++")
+                {
+                    X++;
+                }
+                else
+                {
+                    X--;
+                }
+            }
+            return X;
+        }
+    } 
+}
